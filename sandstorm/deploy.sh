0662a1a416ce2e436cce661352f9bb661489dba5
Igor Drukov <drukovigor@gmail.com>
2021-10-29 12:05:32 +0000

    update

--- sandstorm/deploy.sh
+++ sandstorm/deploy.sh
@@ -6,6 +6,6 @@ mkdir -p ~/Steam/steamapps/common/sandstorm_server/Insurgency/Config/Server
 mv mapcycle.txt ~/Steam/steamapps/common/sandstorm_server/Insurgency/Config/Server/
 
 mkdir -p Insurgency/Saved/Config/LinuxServer
-mv Game.ini  Insurgency/Saved/Config/LinuxServer/
+mv Game.ini  ~/Steam/steamapps/common/sandstorm_server/Insurgency/Saved/Config/LinuxServer/
 
 
